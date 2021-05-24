b6032c0f45e84b012cf14cd396f7c428491042aa
Saleh AlQattan <salehalqattan@outlook.com>
2021-05-24 16:24:29 +0300

    add story

---
+++ story.js
@@ -0,0 +1,9 @@
+let fName = 'saleh';
+let age = '30';
+let hobby = 'Codding';
+
+console.log(
+  `Mr./Mrs. ${fName}, in 33 years, you will be ${
+    age + 33
+  } years old. By then you'll have decades of ${hobby} practice!`
+);
